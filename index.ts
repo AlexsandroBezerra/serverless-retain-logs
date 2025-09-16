c2d75d9e9f76cdd73cc99706135b73ffa43720db
Alexsandro G Bezerra <me@alexsandrobezerra.dev>
2025-09-16 17:43:23 -0300

    Add retain in log groups

--- index.ts
+++ index.ts
@@ -1,5 +1,32 @@
+import { assert } from "node:console";
+
 export default class MyPlugin {
-	constructor() {
+	public readonly hooks: Record<string, () => void>
+	private readonly serverless: any; // TODO: improve type
+
+	constructor(serverless: any) { // TODO: improve type
+		this.hooks = {
+			'before:package:finalize': () => this.execute()
+		}
+
+	}
+
+	private execute() {
+		const cfnTemplate = this?.serverless?.service?.provider?.compiledCloudFormationTemplate
+		assert(cfnTemplate !== undefined, 'CloudFormation template not ready.')
+		assert(Array.isArray(cfnTemplate?.Resources), 'CloudFormation template invalid.')
+
+		const resources: Array<Record<string, unknown>> = cfnTemplate.Resources
+
+		const newResources = resources.map(resource => {
+			if (resource.Type === "AWS::Logs::LogGroup") {
+				return { ...resource, DeletionPolicy: "Retain" }
+			}
+
+			return resource
+		})
+
+		cfnTemplate.Resources = newResources
 	}
 }
 
